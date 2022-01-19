b1df3bba3a7cf0dd6b7ea4cfa027df391d92784c
ariful <ariful.islam@upaybd.com>
2022-01-20 01:09:23 +0600

    added some code

--- app/src/main/java/jp/co/algorithm/BinarySearch.kt
+++ app/src/main/java/jp/co/algorithm/BinarySearch.kt
@@ -14,20 +14,45 @@ class BinarySearch {
 //            } else {
 //                println(pos)
 //            }
-            print(firstBadVersion(3))
+           print( searchInsert(intArrayOf(1,3,5,6), 7))
+          //  print(firstBadVersion(2))
         }
+        //35. Search Insert Position
+        fun searchInsert(nums: IntArray, target: Int): Int {
 
+            var beg = 0
+            var end = nums.size - 1
+            var mid: Int=0
+            while (beg <= end) {
+                mid =  beg+(end - beg) / 2
+                when {
+                    target > nums[mid] -> beg = mid + 1 //
+                    target == nums[mid] -> return mid // found the element
+                    target < nums[mid] -> end = mid-1
+
+                }
+               // print(beg.toString()+"  "+mid+"  "+end)
+            }
+            if(nums[mid]>target)
+            return mid
+            else return mid+1
+
+        }
+//278. First Bad Version
          fun firstBadVersion(n: Int) : Int {
             var beg = 1
             var end = n
             var mid: Int=1
             while (beg < end) {
-                mid =  beg+(end - beg) / 2
+
+                mid = beg+ (end - beg) / 2
                 when {
-                    !isBadVersion(mid)   -> beg = mid + 1 //
-                    isBadVersion(mid)  -> end = mid-1
+                    !isBadVersion(mid)   -> beg = mid+1  //
+                    isBadVersion(mid)  -> end = mid
 
                 }
+                if(beg==end)
+                    return beg
 
             }
              return  mid
